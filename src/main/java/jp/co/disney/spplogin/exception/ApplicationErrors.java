0bb3c69e74d77a81e42a24c51a860fccac1bf8f9
Seiji Takahashi <seiji.takahashi.090@ctc-g.co.jp>
2016-04-13 14:30:16 +0900

    0412 commit

--- src/main/java/jp/co/disney/spplogin/exception/ApplicationErrors.java
+++ src/main/java/jp/co/disney/spplogin/exception/ApplicationErrors.java
@@ -8,8 +8,10 @@ public enum ApplicationErrors {
 
 	INVALID_MAIL_ADDRESS("SPC001", "お使いのメールアドレスは使用できません。：{0}"),
 	INVALID_URL("SPC002", "ご指定のURLは有効ではありません。"),
-	UNEXPECTED("SPC901", "想定外のエラーが発生しました。：{0}"),
-	INVALID_PARAMETER("SPC902", "パラメータの値が正しくありません。");
+	MEMBER_REGIST_FAILED("SPC003", "新規会員登録に失敗しました。:{0}"),
+	INVALID_PARAMETER("SPC902", "パラメータの値が正しくありません。"),
+	INVALID_OPERATION("SPC903", "不正な画面遷移によりこのページへのアクセスが試みられました。"),
+	UNEXPECTED("SPC999", "想定外のエラーが発生しました。：{0}");
 	
 	@Getter
 	private String code;
